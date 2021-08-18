c8add78543ebe1532e5c2fcc12843ec1a12b32fe
Hiroshi Lee <40585737+hiro612@users.noreply.github.com>
2021-08-19 00:27:21 +0800

    2021-08-15

--- MYR.sol
+++ MYR.sol
@@ -7,8 +7,8 @@ import "https://github.com/hiro612/MYR/blob/master/ERC20Detailed.sol";
 contract MYRCoin is Context, ERC20, ERC20Detailed {
     //MYRCoin： 代币的全名
     //MYR：代币的简写
-    //18: 代币小数点位数，代币的最小单位， 5表示我们可以拥有 0.000000000000000001单位个代币
-    constructor () public ERC20Detailed("MYRCoin", "MYR", 18) {
+    //5: 代币小数点位数，代币的最小单位， 5表示我们可以拥有 0.00001单位个代币
+    constructor () public ERC20Detailed("MYRCoin", "MYR", 5) {
         //初始化币，并把所有的币都给部署智能合约的ETH钱包地址
         //2100000000000：代币的总数量
         _mint(_msgSender(), 2100000000000 * (10 ** uint256(decimals())));
